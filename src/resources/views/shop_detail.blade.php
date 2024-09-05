27f5bcd7204a8c9585856ab38e4f11ce2c1b0064
nobuhiro-sato <n.s.mh256@gmail.com>
2024-09-06 07:11:57 +0900

    詳細ページのCSSと®使用

--- src/resources/views/shop_detail.blade.php
+++ src/resources/views/shop_detail.blade.php
@@ -1,34 +1,50 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{asset('css/shop_detail.css')}}">
+@endsection
+
 @section('content')
 <div>
-    <div>
-        <div>
-            <form action="/" method="get">
-                @csrf
-                <input type="submit" value="<">
-            </form>
-            <p>{{$shop['store_name']}}</p>
+    <div class="shop">
+        <div class="shop__detail">
+            <div class="shop__title">
+                <button class="shop__title--button" onclick="location.href='{{ $_SERVER['HTTP_REFERER'] }}'"><</button>
+                <p class="shop__title--name">{{$shop['store_name']}}</p>
+            </div>
+            <div class="shop__about">
+                <img class="shop__about--img" src="{{$shop['shop_image']}}" >
+                <div class="shop__about--category">
+                    <P>#{{$shop->area['name']}}</P>
+                    <p>#{{$shop->genre['name']}}</p>
+                </div>
+                <P>{{$shop['summary']}}</P>
+            </div>
         </div>
-        <div>
-            <img src="{{$shop['shop_image']}}" width="300" height="250">
-            <P>#{{$shop->area['name']}}</P>
-            <p>#{{$shop->genre['name']}}</p>
-            <P>{{$shop['summary']}}</P>
-        </div>
-        <div>
+        <div class="shop__reservation">
             @if(!empty($today_date))
-            <form action="/reservation" method="post">
+            <form class="shop__form" action="/reservation" method="post">
+            @else
+            <form class="shop__form" action="/update" method="post">
+            @endif
                 @csrf
-                <div>
-                    <p>予約</p>
+                <div class="shop__reservation--input">
+                    @if(!empty($today_date))
+                    <p class="shop__reservation--title">予約</p>
+                    @else
+                    <p class="shop__reservation--title">予約変更</p>
+                    @endif
                     @if (Auth::check())
                     <input type="hidden" name="user_id" value="{{Auth::id()}}" />
                     @endif
                     <input type="hidden" name="shop_id" value="{{$shop['id']}}" />
-                    <input type="date" name="date" oninput="finalConfirmation()" id="date" value="{{$today_date}}"/>
-                    <input type="time" name="time" oninput="finalConfirmation()" id="time" min="09:00" max="18:00" />
-                    <select name="number" oninput="finalConfirmation()" id="number" >
+                    @if(!empty($today_date))
+                    <input class="shop__reservation--date" type="date" name="date" oninput="finalConfirmation()" id="date" value="{{$today_date}}"/>
+                    @else
+                    <input class="shop__reservation--date" type="date" name="date" value="{{$reservation['date']}}"/>
+                    @endif
+                    <input class="shop__reservation--time" type="time" name="time" oninput="finalConfirmation()" id="time" min="09:00" max="18:00" />
+                    <select class="shop__reservation--number" name="number" oninput="finalConfirmation()" id="number" >
                         <option value="1">1人</option>
                         <option value="2">2人</option>
                         <option value="3">3人</option>
@@ -36,65 +52,33 @@
                         <option value="5">5人</option>
                     </select>
                 </div>
-                <div>
-                    <div>
+                <div class="shop__confirmation">
+                    <div class="shop__confirmation--up">
                         <p>Shop</p>
                         <p>{{$shop['store_name']}}</p>
                     </div>
-                    <div>
+                    <div class="shop__confirmation--item">
                         <p>Date</p>
-                        <p id="date"></p>
+                        <p id="date">日にち</p>
                     </div>
-                    <div>
+                    <div class="shop__confirmation--item">
                         <p>Time</p>
-                        <p id="time"></p>
+                        <p id="time">時間</p>
                     </div>
-                    <div>
+                    <div class="shop__confirmation--down">
                         <p>Number</p>
-                        <p id="number"></p>
+                        <p id="number">人数</p>
                     </div>
                 </div>
-                <div>
-                    <input type="submit" value="予約する" />
-                </div>
-                @else
-                <form action="/update" method="post">
-                @csrf
-                <div>
-                    <p>予約変更</p>
-                    @if (Auth::check())
-                    <input type="hidden" name="user_id" value="{{Auth::id()}}" />
+                <div class="shop__submit">
+                    @if(!empty($today_date))
+                    <input class="shop__submit--button" type="submit" value="予約する" />
+                    @else
+                    <input class="shop__submit--button" type="submit" value="予約変更" />
                     @endif
-                    <input type="hidden" name="shop_id" value="{{$shop['id']}}" />
-                    <input type="date" name="date" value="{{$reservation['date']}}"/>
-                    <input type="time" name="time" min="09:00" max="18:00" />
-                    <select name="number"  >
-                        <option value="1">1人</option>
-                        <option value="2">2人</option>
-                        <option value="3">3人</option>
-                        <option value="4">4人</option>
-                        <option value="5">5人</option>
-                    </select>
-                </div>
-                <div class="final__confirmation">
-                    <p>Shop</p>
-                    <p>Date</p>
-                    <p>Time</p>
-                    <p>Number</p>
-                </div>
-                <div>
-                    <input type="hidden" name="id" value="{{$reservation['id']}}" />
-                    <input type="submit" value="予約変更" />
                 </div>
             </form>
-            @endif
         </div>
     </div>
 </div>
-
-<script>
-    function finalConfirmation(){
-        document.getElementById()
-    }
-</script>
 @endsection
