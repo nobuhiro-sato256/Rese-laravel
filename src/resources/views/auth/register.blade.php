0dfb379e160a148de901cf7d6a67fbbb9072260c
nobuhiro-sato <n.s.mh256@gmail.com>
2024-09-17 22:57:57 +0900

    バリデーションのCSSを修正

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -14,33 +14,33 @@
             <form class="register__form" action="/register" method="post">
                 @csrf
                 <div class="register__form-box">
-                    @error('name')
-                    @foreach ($errors->get('name') as $error)
-                    <p>{{ $error }}</p>
-                    @endforeach
-                    @enderror
                     <div class="register__form-item">
                         <i class="bi bi-person-fill"></i>
                         <input type="text" name="name" placeholder="Username" value="{{ old('name') }}" />
                     </div>
-                    @error('email')
-                    @foreach ($errors->get('email') as $error)
-                    <p>{{ $error }}</p>
+                    @error('name')
+                    @foreach ($errors->get('name') as $error)
+                    <p class="register__error">{{ $error }}</p>
                     @endforeach
                     @enderror
                     <div class="register__form-item">
                         <i class="bi bi-envelope-fill"></i>
                         <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" />
                     </div>
-                    @error('password')
-                    @foreach ($errors->get('password') as $error)
-                    <p>{{ $error }}</p>
+                    @error('email')
+                    @foreach ($errors->get('email') as $error)
+                    <p class="register__error">{{ $error }}</p>
                     @endforeach
                     @enderror
                     <div class="register__form-item">
                         <i class="bi bi-lock-fill"></i>
                         <input type="password" name="password" placeholder="Password" />
                     </div>
+                    @error('password')
+                    @foreach ($errors->get('password') as $error)
+                    <p class="register__error">{{ $error }}</p>
+                    @endforeach
+                    @enderror
                 </div>
                 <div class="register__form-button">
                     <input type="submit" value="登録" />
